6d44fd8ef60db74b0b197c29e478e510ab4f0f92
vanquiche <stevevang2010@hotmail.com>
2021-07-18 22:07:05 -0700

    update routing

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var mongoDB =
   'mongodb+srv://admin:123@cluster0.po5hy.mongodb.net/local_libray?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+//Import routes for "catalog" area of site
 
 var app = express();
 
@@ -29,14 +31,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
