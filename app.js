9e3dabdd08701a0c822bf816c86e21729ee539cf
vanquiche <stevevang2010@hotmail.com>
2021-07-22 20:02:19 -0700

    add delete functunality

--- app.js
+++ app.js
@@ -15,13 +15,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
+var helmet = require('helmet');
 //Import routes for "catalog" area of site
 
 var app = express();
 
+app.use(helmet());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(compression());
 
 app.use(logger('dev'));
 app.use(express.json());
